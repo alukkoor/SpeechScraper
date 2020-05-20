ed7dba08754b8afadc0168c9dc9e6cf1f1dd650b
Amith Lukkoor <amithlukkoor@gmail.com>
2020-05-19 18:06:14 -0700

    Write function

--- speech_scraper.py
+++ speech_scraper.py
@@ -8,7 +8,7 @@ def get_links(count=5):
     import re
     
     if count > 37:
-        raise ValueError
+        count = 37
     res = []
     for i in range(1, count+1):
         url = 'https://www.rev.com/blog/transcript-tag/us-state-governor-coronavirus-briefing-transcripts/page/' + str(i)
@@ -33,21 +33,54 @@ def scrape_speech(url):
     speech = [soup.title.get_text()]
     for br in soup.find_all('br'):
         if br.nextSibling:
-            speech.append(br.next_sibling)
+            speech.append(str(br.next_sibling))
     return speech
 
 
+def write_txt(transcript):
+    '''
+    Write transcript to text file
+    '''
+    import os
+    dirname = 'transcripts/'
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
+
+    title = transcript[0]
+    states = [
+        'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 
+        'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 
+        'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 
+        'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 
+        'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 
+        'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 
+        'New Jersey', 'New Mexico', 'New York', 'North Carolina', 
+        'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 
+        'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 
+        'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 
+        'West Virginia', 'Wisconsin', 'Wyoming']
+
+    for state in states:
+        if state in title:
+            name = state
+            break
+
+    filename = dirname + name + '1.txt'
+    num = 1
+    while os.path.exists(filename) :
+        num += 1
+        filename = dirname + name + str(num) + '.txt'
+
+    with open(filename, 'w') as file:
+        transcript = '\n'.join(transcript)
+        file.write(transcript)
+
 def main():
-    '''
-
-    TODO: add functionality to write speeches to text files
-
-    '''
-
+    
     # Test scrape with J.B. Pritzker
     url = 'https://www.rev.com/blog/transcripts/illinois-governor-j-b-pritzker-coronavirus-press-conference-transcript-may-15'
     transcript = scrape_speech(url)
-    print(*transcript, sep = '\n')
+    write_txt(transcript)
     
     # # Scrape all transcripts included in num pages
     # num = 5
